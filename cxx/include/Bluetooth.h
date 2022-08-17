ab7719f624fd06a76003bdcd88de7ed2835783fc
mobergmann <mobergmann@pm.me>
2022-08-17 11:04:59 +0200

    added bluetooth class

---
+++ cxx/include/Bluetooth.h
@@ -0,0 +1,14 @@
+//
+// Created by mobergmann on 17.08.22.
+//
+
+#ifndef LED_CUBE_BLUETOOTH_H
+#define LED_CUBE_BLUETOOTH_H
+
+
+class Bluetooth {
+
+};
+
+
+#endif //LED_CUBE_BLUETOOTH_H
---
+++ cxx/src/Bluetooth.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mobergmann on 17.08.22.
+//
+
+#include "../include/Bluetooth.h"
