fde713cc3402380799d704a79e14536b47a5b48d
mobergmann <mobergmann@pm.me>
2022-08-23 15:29:41 +0200

    implemented layer test in c++

--- cxx/test.cpp
+++ cxx/test.cpp
@@ -13,234 +13,151 @@
 // Files
 #include "Button.h"
 
+
 using values_t = std::array<bool, 5>;
 using lines_t = std::array<values_t, 5>;
 using layers_t = std::array<lines_t, 5>;
 
-namespace fs = std::filesystem;
-
-
-struct Frame
+class Main
 {
-    /// the time in milliseconds, how long the frame should be visible
-    unsigned int frame_time;
-    /// for each layer a stream of booleans, encoding which led is on
-    layers_t data;
-};
+private:
+#pragma region lines
+    /// reference to the lines chip
+    gpiod::chip chip;
 
+    /// pins for each layer toggle
+    std::array<gpiod::line, 5> layers;
 
-class A
-{
+#pragma region led
+    /// reset pin
+    gpiod::line pin_reset;
+
+    /// shift clock pin
+    gpiod::line pin_shift;
+
+    /// store clock pin
+    gpiod::line pin_store;
+
+    /// datain pin
+    gpiod::line pin_datain;
+
+    /// special pin, which cannot be accessed by shifting
+    gpiod::line pin_special;
+#pragma endregion
+#pragma endregion
+
 public:
-    /// a list of frames, each representing a current state of the cube
-    std::vector<Frame> frames;
-
-    #pragma region file management
-    /// a list of all files in the data folder
-    std::vector<std::string> files;
-
-    /// stores the current configuration file
-    std::string current_file;
-    #pragma endregion
-
-
-    void poll(const std::function<void()>& callback)
+    Main()
     {
-        // run callback function
-        callback();
+        // init chip
+        chip = gpiod::chip("gpiochip0", gpiod::chip::OPEN_BY_NAME);
+
+#pragma region aquire lines
+#pragma region layers
+        // save layers to array
+        layers = {
+                chip.get_line(20),
+                chip.get_line(21),
+                chip.get_line(23),
+                chip.get_line(24),
+                chip.get_line(25)
+        };
+
+        // initialize layers
+        for (auto &layer: layers)
+        {
+            layer.request({layer.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+        }
+        std::cout << "All Layers acquired" << std::endl;
+#pragma endregion
+
+#pragma region led
+        // reset pin setup (pull down)
+        pin_reset = chip.get_line(18);
+        pin_reset.request({pin_reset.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 1);
+        std::cout << "Reset pin acquired" << std::endl;
+
+        // shift pin setup (pull down)
+        pin_shift = chip.get_line(14);
+        pin_shift.request({pin_shift.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+        std::cout << "Shift pin acquired" << std::endl;
+
+        // store pin setup (pull down)
+        pin_store = chip.get_line(15);
+        pin_store.request({pin_store.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+        std::cout << "Store pin acquired" << std::endl;
+
+        // datain pin setup (pull down)
+        pin_datain = chip.get_line(12);
+        pin_datain.request({pin_datain.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+        std::cout << "Datain pin acquired" << std::endl;
+
+        // special pin setup (pull down)
+        pin_special = chip.get_line(13);
+        pin_special.request({pin_special.name(), gpiod::line_request::DIRECTION_OUTPUT, 0}, 0);
+        std::cout << "Special pin acquired" << std::endl;
+#pragma endregion
+#pragma endregion
     }
 
-    static void parse_layout(A* m)
+private:
+#pragma region line controll abstraction methods
+    void reset()
     {
-        std::vector<Frame> _frames;
-
-        // load from file
-        std::ifstream stream(m->current_file);
-
-        nlohmann::json file;
-        stream >> file;
-
-        // parse json
-        for (auto &_frame: file["frames"])
-        {
-            Frame frame{};
-
-            // frame time
-            frame.frame_time = _frame["frame-time"];
-
-            // layer data
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    for (int k = 0; k < 5; k++)
-                    {
-                        const auto &value = _frame["layers"][i][j][k];
-//                        std::cout << value << std::endl;
-                        frame.data[i][j][k] = (bool)value;
-                    }
-                }
-            }
-
-            // reverse each lain and each
-            for (auto &layer: frame.data)
-            {
-                for (auto &lain: layer)
-                {
-                    std::reverse(std::begin(lain), std::end(lain));
-                }
-            }
-
-            _frames.push_back(frame);
-        }
-
-        m->frames = _frames;
+        pin_reset.set_value(0);
+        pin_reset.set_value(1);
+        pin_special.set_value(0);
     }
 
-    void update_file_list()
+    void shift()
     {
-        std::string tmp = current_file;
-
-        files.clear();
-
-        // search all new files
-        std::string data_dir_path = fs::path(getenv("HOME")) / ".led-cube" / "custom";
-        std::string default_dir_path = fs::path(getenv("HOME")) / ".led-cube" / "default";
-        // add default cube configurations
-        for (const auto &file: fs::recursive_directory_iterator(default_dir_path))
-        {
-            std::cout << "Found file: " << file.path() << std::endl;
-            files.push_back(file.path());
-        }
-        // add custom cube configurations
-        for (const auto &file: fs::recursive_directory_iterator(data_dir_path))
-        {
-            std::cout << "Found file: " << file.path() << std::endl;
-            files.push_back(file.path());
-        }
-
-        // search element in list, if not exits use fallback option
-        current_file = files[0]; // fallback option
-        for (const auto &val: files)
-        {
-            if (val == tmp)
-            {
-                current_file = val;
-                break;
-            }
-        }
+        pin_shift.set_value(1);
+        pin_shift.set_value(0);
     }
 
-    void next()
+    void store()
     {
-        // if only one or fewer elements in list => no scrolling possible
-        if (files.size() <= 1)
-        {
-            return;
-        }
-
-        // search element in list, if not exits use fallback option
-        std::string tmp = current_file;
-        current_file = files[0]; // fallback option
-        for (int i = 0; i < files.size(); ++i)
-        {
-            const auto &it = files[i];
-
-            if (it == tmp)
-            {
-                // when at end, the next element is the beginning of the list
-                if (i == files.size()-1)
-                {
-                    // current_file = files[0]; // this is the fallback option, therefore change nothing
-                }
-                    // when found use previous element as new one
-                else
-                {
-                    current_file = files[i+1];
-                }
-                break;
-            }
-        }
-        std::cout << "New Configuration: " << current_file << std::endl;
+        pin_store.set_value(1);
+        pin_store.set_value(0);
     }
+#pragma endregion
 
-    static void previous(A* m)
-    {
-        // if only one or fewer elements in list => no scrolling possible
-        if (m->files.size() <= 1)
-        {
-            return;
-        }
-
-        // search element in list, if not exits use fallback option
-        std::string tmp = m->current_file;
-        m->current_file = m->files[0]; // fallback option
-        for (int i = 0; i < m->files.size(); ++i)
-        {
-            const auto &it = m->files[i];
-
-            if (it == tmp)
-            {
-                // when at beginning, the previous element is the beginning of the list
-                if (i == 0)
-                {
-                    m->current_file = m->files[m->files.size()-1];
-                }
-                    // when found use previous element as new one
-                else
-                {
-                    m->current_file = m->files[i-1];
-                }
-                break;
-            }
-        }
-        std::cout << "New Configuration: " << m->current_file << std::endl;
-    }
-
-    void set_leds(const layers_t &frame_data)
-    {
-        for (const auto &layer: frame_data)
-        {
-            std::cout << "\t\tlayer:" << std::endl;
-            for (const auto &line: layer)
-            {
-                std::cout << "\t\t\t";
-                for (const auto &value: line)
-                {
-                    std::cout << value << ", ";
-                }
-                std::cout << std::endl;
-            }
-        }
-        std::cout << std::endl << std::endl;
-    }
-
+public:
     void loop()
     {
-        std::cout << "Config" << std::endl;
-        for (auto &frame: frames)
+        for (const auto& layer: layers)
         {
-            // poll for possible button events
-            poll([&](){
-                previous(this);
-                parse_layout(this);
-            });
-
-            std::cout << "\tFrame" << std::endl;
-            set_leds(frame.data);
+            layer.set_value(1);
+            for (int i = 0; i < 25; ++i)
+            {
+                if (i == 24)
+                {
+                    pin_special.set_value(1);
+                }
+                else
+                {
+                    pin_datain.set_value(1);
+                    shift();
+                }
+            }
+            store();
+            layer.set_value(0);
         }
     }
 };
 
 int main()
 {
-    A m;
-    m.update_file_list();
-
-    while (true)
+    try
     {
-        A::previous(&m);
-        A::parse_layout(&m);
-        m.loop();
+        Main m;
+        while (true)
+        {
+            m.loop();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "failure: " << e.what() << std::endl;
     }
 }
