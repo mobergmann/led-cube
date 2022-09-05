620003c3625673b6cee8006395a1e03fc32b84de
mobergmann <mobergmann@pm.me>
2022-09-05 22:03:25 +0200

    finalized install script

--- cxx/install/build.sh
+++ cxx/install/build.sh
@@ -1,40 +1,91 @@
 # only run as root (source: https://electrictoolbox.com/check-user-root-sudo-before-running/)
 if [ `whoami` == root ]; then
-    echo Please don\'t run this script as root or using sudo
+    echo Please run this script as root or using sudo
     exit
 fi
 
-# region declare variables
-START_DIR=$(pwd)
+# todo exit if not on raspi
 
+# declare variables
+START_DIR=$(pwd)
 # get path of script (if called from other location)
 # source: https://stackoverflow.com/a/1638397/11186407
 SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT_DIR=$(dirname "$SCRIPT")
+BUILD_DIR="$SCRIPT_DIR/../build"
+BINARIES_DIR="$SCRIPT_DIR/bin"
 
-BUILD_DIR="$SCRIPTPATH/../build"
-
-BINARIES_DIR="$SCRIPTPATH/binaries"
-# endregion
 
 # install dependencies
-sudo apt install build-essential cmake libgpiod-dev -y
-# todo if on raspi/ apt available continue
+#sudo apt install build-essential cmake libgpiod-dev -y
 
 # build
-echo "DEBUG: $BUILD_DIR"
-cd "$SCRIPTPATH"/.. || exit
-cmake -B "$BUILD_DIR/"
-cmake --build "$BUILD_DIR/"
+echo -n "Building Project ... "
+{
+    cd "$SCRIPT_DIR"/.. || exit
+    cmake -B "$BUILD_DIR/"
+    cmake --build "$BUILD_DIR/"
+} &> /dev/null
+echo "Done"
 
 # move binaries
-mkdir -p "$BINARIES_DIR"
-cp "$BUILD_DIR"/led_cube "$BINARIES_DIR"/
+echo -n "Moving binaries ... "
+{
+    mkdir -p "$BINARIES_DIR"
+    cp "$BUILD_DIR/led_cube" "$BINARIES_DIR/"
+} &> /dev/null
+echo "Done"
 
 # download dependency library
-cd "$BINARIES_DIR"/ || exit
-apt download libgpiod2
+echo -n "Downloading dependency ... "
+{
+    cd "$BINARIES_DIR"/ || exit
+    #apt download libgpiod2
+    # wget http://ftp.de.debian.org/debian/pool/main/libg/libgpiod/libgpiod2_1.6.2-1_armel.deb
+} &> /dev/null
+echo "Done"
+
+
+# ask user if also want to install to usb
+# source: https://stackoverflow.com/a/27875395/11186407
+echo "Do you want to create a install stick (please make sure you have an USB inserted as /dev/sda1)? [Y|n]"
+old_stty_cfg=$(stty -g)
+stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg # Careful playing with stty
+if echo "$answer" | grep -iq "^y" ; then
+    echo "Yes"
+else
+    echo "No"
+    cd "$START_DIR"/ || exit
+    exit
+fi
+
+
+
+# declare variables
+MOUNT_DIR=/mnt
+USB_DIR="$MOUNT_DIR/cube"
+
+
+echo -n "Moving files to usb ... "
+
+# mount and give access rights
+sudo mount /dev/sda1 "$MOUNT_DIR/"
+sudo mkdir -p "$MOUNT_DIR"
+
+# move configurations
+sudo cp -r "$SCRIPT_DIR/configurations" "$USB_DIR/"
+
+# move assets
+sudo cp -r "$SCRIPT_DIR/assets" "$USB_DIR/"
+sudo mv "$USB_DIR/assets/install.sh" "$USB_DIR/install.sh"
+
+# move binaries
+sudo cp -r "$SCRIPT_DIR/bin" "$USB_DIR/"
+
+
+sudo umount "$MOUNT_DIR/"
+echo "Done"
+
 cd "$START_DIR"/ || exit
 
-
-# todo ask user if want to install to usb
+echo "Finished creating install USB. Stick the USB into the Raspi and then run the install script."
